6aac9b804e170b6796f1d2b19fb19d99e0e18f39
anjiru81 <anjiru81@gmail.com>
2017-04-13 17:49:53 +0200

    Primer envio

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/**
+ * jTinder initialization
+ */
+$("#tinderslide").jTinder({
+	// dislike callback
+    onDislike: function (item) {
+	    // set the status text
+        $('#status').html('Dislike image ' + (item.index()+1));
+    },
+	// like callback
+    onLike: function (item) {
+	    // set the status text
+        $('#status').html('Like image ' + (item.index()+1));
+    },
+	animationRevertSpeed: 200,
+	animationSpeed: 400,
+	threshold: 1,
+	likeSelector: '.like',
+	dislikeSelector: '.dislike'
+});
+
+/**
+ * Set button action to trigger jTinder like & dislike.
+ */
+$('.actions .like, .actions .dislike').click(function(e){
+	e.preventDefault();
+	$("#tinderslide").jTinder($(this).attr('class'));
+});
+
+$('.pane5').prepend('<iframe class="video container-fluid" src="https://flashservice.xvideos.com/embedframe/16858407" frameborder=0 width=510 height=400 scrolling=no allowfullscreen=allowfullscreen></iframe>');
